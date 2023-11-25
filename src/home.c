852258529097e1d728e16da1adb95b317736d910
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2023-11-25 11:34:11 +0530

    revert previous  commit

--- src/home.c
+++ src/home.c
@@ -325,13 +325,15 @@ powerbar_press_gesture_stopped (PhoshHome *self)
 }
 
 static void
-on_powerbar_pressed (PhoshHome *self)
+on_powerbar_pressed (PhoshHome *self, PhoshOskManager *osk, PhoshShell *shell)
 {
   gboolean osk_is_available, osk_current_state, osk_new_state;
  
   PhoshDragSurfaceState drag_state = phosh_drag_surface_get_drag_state (PHOSH_DRAG_SURFACE (self));
   g_return_if_fail (PHOSH_IS_HOME (self));
-
+  shell = phosh_shell_get_default ();
+  self->osk = phosh_shell_get_osk_manager (shell);
+  
   osk_is_available = phosh_osk_manager_get_available (self->osk);
   osk_current_state = phosh_osk_manager_get_visible (self->osk);
   osk_new_state = osk_current_state;
